9302c1d9f9659306cf4fa851ff1a7e65999f2534
LorgioLobster <lorgio@projectlobster.com>
2022-01-12 11:20:22 +0100

    update data CORS

--- src/api.js
+++ src/api.js
@@ -1,25 +1,22 @@
 const url = "https://api.coincap.io/v2";
 
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  mode: 'cors',
+  header: {
+    'Access-Control-Allow-Origin': '*'
+  }
+};
+
 function getAssets() {
-  return fetch(`${url}/assets?limit=20`, {
-    mode: "cors",
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-    },
-  })
+  return fetch(`${url}/assets?limit=20`, requestOptions)
     .then((res) => res.json())
     .then((res) => res.data);
 }
 
 function getAsset(coin) {
-  return fetch(`${url}/assets/${coin}`, {
-    mode: "cors",
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-    },
-  })
+  return fetch(`${url}/assets/${coin}`, requestOptions)
   .then(response => response.json())
   .then((response) => {
     return response.data;
@@ -33,20 +30,19 @@ function getAssetHistory(coin) {
   const start = now.getTime();
 
   return fetch(
-    `${url}/assets/${coin}/history?interval=h1&start=${start}&end=${end}`
-  )
+    `${url}/assets/${coin}/history?interval=h1&start=${start}&end=${end}`,requestOptions)
     .then((res) => res.json())
     .then((res) => res.data);
 }
 
 function getMarkets(coin) {
-  return fetch(`${url}/assets/${coin}/markets?limit=5`)
+  return fetch(`${url}/assets/${coin}/markets?limit=5`, requestOptions)
     .then((res) => res.json())
     .then((res) => res.data);
 }
 
 function getExchange(id) {
-  return fetch(`${url}/exchanges/${id}`)
+  return fetch(`${url}/exchanges/${id}`, requestOptions)
     .then((res) => res.json())
     .then((res) => res.data);
 }
--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -4,7 +4,9 @@
       <div class="flex flex-col sm:flex-row justify-around items-center">
         <div class="flex flex-col items-center">
           <img
-            :src="`https://static.coincap.io/assets/icons/${asset.symbol.toLowerCase()}@2x.png`"
+            :src="
+              `https://static.coincap.io/assets/icons/${asset.symbol.toLowerCase()}@2x.png`
+            "
             :alt="asset.name"
             class="w-20 h-20 mr-5"
           />
@@ -43,30 +45,33 @@
           </ul>
         </div>
 
-        <div class="my-10 sm:mt-0 flex flex-col justify-center text-center">
-
-        </div>
+        <div
+          class="my-10 sm:mt-0 flex flex-col justify-center text-center"
+        ></div>
       </div>
-          <line-chart
+      <line-chart
         class="my-10"
         :colors="['orange']"
         :min="min"
         :max="max"
-        :data="history.map(h => [h.date, parseFloat(h.priceUsd).toFixed(2)])"
+        :data="history.map((h) => [h.date, parseFloat(h.priceUsd).toFixed(2)])"
       />
     </template>
     <h3 class="text-xl my-10">Mejores Ofertas de Cambio</h3>
-      <table>
-        <tr v-for="detail in markets" :key="`${detail.exchangeId}, ${detail.priceUsd}`" class="border-b">
-          <td>
-            <b>{{detail.exchangeId}}</b>
-          </td>
-          <td>{{detail.priceUsd | dollar}}</td>
-          <td>{{detail.baseSymbol}} / {{detail.quotesSymbol}}</td>
-        </tr>
-      </table>
+    <table>
+      <tr
+        v-for="detail in markets"
+        :key="`${detail.exchangeId}, ${detail.priceUsd}`"
+        class="border-b"
+      >
+        <td>
+          <b>{{ detail.exchangeId }}</b>
+        </td>
+        <td>{{ detail.priceUsd | dollar }}</td>
+        <td>{{ detail.baseSymbol }} / {{ detail.quotesSymbol }}</td>
+      </tr>
+    </table>
   </div>
-
 </template>
 
 <script>
@@ -74,7 +79,6 @@ import api from "../api.js";
 
 export default {
   name: "CoinDetail",
-
 
   data() {
     return {
@@ -108,20 +112,22 @@ export default {
   },
 
   methods: {
-    getWebSite(exchange){
-      return api.getExchange(exchange.exchangId)
-      .then(res => {
-          this.$set(exchange, 'url', res.exchangeUrl)
-      })
+    getWebSite(exchange) {
+      return api.getExchange(exchange.exchangId).then((res) => {
+        this.$set(exchange, "url", res.exchangeUrl);
+      });
     },
     getCoin() {
       const id = this.$route.params.id;
-      Promise.all([api.getAsset(id), api.getAssetHistory(id), api.getMarkets(id)]).then(
-        ([assets, history, markets]) => {
-          (this.asset = assets), (this.history = history),
-          (this.markets = markets );
-        }
-      );
+      Promise.all([
+        api.getAsset(id),
+        api.getAssetHistory(id),
+        api.getMarkets(id),
+      ]).then(([assets, history, markets]) => {
+        (this.asset = assets),
+          (this.history = history),
+          (this.markets = markets);
+      });
     },
   },
 };
